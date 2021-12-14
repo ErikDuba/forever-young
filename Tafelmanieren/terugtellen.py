b9574bd4c27d379f8f7cf066c0dfc12a0063e163
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-12-14 12:39:45 +0100

    Update terugtellen.py

--- Tafelmanieren/terugtellen.py
+++ Tafelmanieren/terugtellen.py
@@ -1,18 +1,24 @@
 import time
 import os
+import sys
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1./10)
 def clear():
     command = 'cls'
     os.system(command)
 
 
-confirmation = input('Do you want to launch \'Het raketje\'? yes/no: ')
+confirmation = input(slowprint('Do you want to launch \'Het raketje\'? yes/no: '))
 clear()
 if confirmation == 'yes':
     for i in range(30, -1, -1):
         clear()
         print(i)
-        time.sleep(1)
-    print('\'Het raketje\' is on it\'s million mile journey to Saturn! ')
+        time.sleep(0.5)
+    slowprint('\'Het raketje\' is on it\'s million mile journey to Saturn! ')
 elif confirmation == 'no':
-    print('The rocket exploded and 721 people died...')
+    slowprint('The rocket exploded and 721 people died...')
 input('')
